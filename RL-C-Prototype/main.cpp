74ddeb1d3af5bcd835d86b22c77bb2ff730aec58
arunkumarbhattar <bhattar1@purdue.edu>
2022-01-29 01:58:56 -0500

    minor changes

--- RL-C-Prototype/main.cpp
+++ RL-C-Prototype/main.cpp
@@ -30,6 +30,7 @@ bool execute_unchecked_function(char* func_name, int* a, int* b, int* result)
 	 * and will be accessible globall across this c++ glue code
 	 */
         rlbox::rlbox_sandbox<rlbox_noop_sandbox> sandbox;
+	sandbox.create_sandbox();
 	/*In the future, as we have a large number of C Unchecked libraries,
 	 We can have a std::multimap<string func_name, string library_name.so> to 
 	 return us the exact library we wish to open for this function call
@@ -68,16 +69,21 @@ bool execute_unchecked_function(char* func_name, int* a, int* b, int* result)
 	 * "typeof" will be used find and insert the type (T) of varargs 
 	 * into the malloc_in_sandbox<T>	
        	 */
+
+	 /*
 	 tainted<int*, rlbox_noop_sandbox> t_a = sandbox.malloc_in_sandbox<int>();
 	 tainted<int*, rlbox_noop_sandbox> t_b = sandbox.malloc_in_sandbox<int>();
+	 tainted<int*, rlbox_noop_sandbox> t_result = sandbox.malloc_in_sandbox<int>();
+	 */
+	 auto t_a = sandbox.malloc_in_sandbox<int>();
+	 auto t_b = sandbox.malloc_in_sandbox<int>();
+	 auto t_result = sandbox.malloc_in_sandbox<int>();
 	 *t_a = *a;
 	 *t_b = *b;
 	 tainted<int*, rlbox_noop_sandbox> tainted_result = sandbox.malloc_in_sandbox<int>(1);
 	 cout << "Calling Unchecked function thorugh Sandbox...\n";
-	 /* 
-	 sandbox.invoke_sandbox_function(unchecked_func, (int*)tainted_a, (int*)tainted_b,
-		(int*)tainted_result);
-	 */
+	 sandbox.invoke_sandbox_function(unchecked_func, t_a, t_b,
+		t_result);
 	 auto result_t = tainted_result.copy_and_verify([](std::unique_ptr<int > val){
 	 	if(*val.get() <100)
 		{
@@ -108,82 +114,3 @@ tainted<int, rlbox_noop_sandbox> hello_cb(rlbox_sandbox<rlbox_noop_sandbox>& _,
   printf(" and the string is: %s\n", checked_string.get());
   return 1;
 }
-
-/*
-int main(int argc, char const *argv[]) {
-    using std::cout;
-    using std::cerr;
-  
-    //We attempt to use dlopen to get the function reference with just a string
-    {
-    	cout << "C++ dlopen demo\n\n";
-
-    	// open the library
-    	cout << "Opening so_header.so...\n";
-    	void* handle = dlopen("./mylib.so", RTLD_LAZY);
-
-    	if (!handle) {
-        cerr << "Cannot open library: " << dlerror() << '\n';
-        return 1;
-    	}
-
-    	// load the symbol
-   	 cout << "Loading symbol hello...\n";
-    	typedef void (*hello_t)();
-
-    	// reset errors
-    	dlerror();
-	
-	//define in include/mylib1/
-    	std::string yourfunc("hello"); // Here is your function
-
-    	hello_t hello = (hello_t) dlsym(handle, yourfunc.c_str());
-    	const char *dlsym_error = dlerror();
-    	if (dlsym_error) {
-        cerr << "Cannot load symbol 'hello': " << dlsym_error <<
-            '\n';
-        dlclose(handle);
-        return 1;
-    	}
-    }
-  //Now we attempt to sandbox the dlsym returned function
-  // Create a new sandbox
-  extern rlbox::rlbox_sandbox<rlbox_noop_sandbox> sandbox;
-  sandbox.create_sandbox();
-  printf("*****************Purely Called from Library 1\n");
-  // call the library hello function
-  sandbox.invoke_sandbox_function(hello);
-
-  // call the add function and check the result:
-  sandbox.invoke_sandbox_function(add, 3, 4).copy_and_verify([](unsigned ret){
-      printf("Adding (3+4) by calling CheckedC function add from Lib1 = %d\n", ret);
-      return ret == 7;
-  });
-  // call the library echo function by passing tainted string
-  const char* helloStr = "Printing this from main.cpp!\n";
-  size_t helloSize = strlen(helloStr) + 1;
-  auto taintedStr = sandbox.malloc_in_sandbox<char>(helloSize);
-  std::strncpy(taintedStr.unverified_safe_pointer_because(helloSize, "writing to region"), helloStr, helloSize);
-  cout<<"The string * "<<helloStr<<" * has been loaded to sandbox memory @ address "<< (void*)&taintedStr<<endl;
-  sandbox.invoke_sandbox_function(echo, taintedStr);
-
-  //trying to call the above function, but this time I try to crash program from within lib1
-  const char* CrashStr = "HESOYAM!";
-  size_t CrashSize = strlen(helloStr) + 1;
-  auto taintedCrashStr = sandbox.malloc_in_sandbox<char>(CrashSize);
-  std::strncpy(taintedCrashStr.unverified_safe_pointer_because(CrashSize, "writing to region"),CrashStr,CrashSize);
-  //sandbox.invoke_sandbox_function(echo, taintedCrashStr);
-
-  printf("*****************Purely Called from Library 2\n");
-  //register a callback and call it 
-  auto cb = sandbox.register_callback(hello_cb);
-  sandbox.invoke_sandbox_function(call_cb, cb);
-  cb.unregister();
-  printf("************Calling a Unchecked Function***\n");
-
-  // destroy sandbox
-  sandbox.destroy_sandbox();
-    return 0;
-}
-
-*/
