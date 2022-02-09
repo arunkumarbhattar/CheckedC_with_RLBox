b6fe424cc57baf6d143190aba001586f3c2a963e
arunkumarbhattar <bhattar1@purdue.edu>
2022-02-08 23:04:54 -0500

    Adding emscripten for converting C to WASM

---
+++ RL-C-Prototype/examples/legacy-c/wasm_sandboxing/build/CMakeFiles/img_lib_wasm.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/img_lib_wasm.dir/wasm_prebuilt/lib_wasm.c.o"
+  "CMakeFiles/img_lib_wasm.dir/wasmrt/wasm-rt-impl.c.o"
+  "CMakeFiles/img_lib_wasm.dir/wasmrt/wasm-rt-os-unix.c.o"
+  "CMakeFiles/img_lib_wasm.dir/wasmrt/wasm-rt-os-win.c.o"
+  "CMakeFiles/img_lib_wasm.dir/wasmrt/wasm-rt-runner.c.o"
+  "CMakeFiles/img_lib_wasm.dir/wasmrt/wasm-rt-wasi.c.o"
+  "libimg_lib_wasm.a"
+  "libimg_lib_wasm.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/img_lib_wasm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
