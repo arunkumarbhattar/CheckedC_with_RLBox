0c9ecef75efc8e1d6320ad068838eaba8fdbe787
arunkumarbhattar <bhattar1@purdue.edu>
2022-02-25 00:35:39 -0500

    minor changes

--- RL-CC-Prototype/unchecked_to_checked/struct_marshalling/safe_library.c
+++ RL-CC-Prototype/unchecked_to_checked/struct_marshalling/safe_library.c
@@ -5,26 +5,26 @@
 #include "../../../RL-C-Prototype/rlbox_core_engine/mylibheader.hpp"
 
 
-void get_image_bytes(char* input_stream)
+void get_image_bytes(_Array_ptr<char> input_stream)
 {
-	char* temp = "This is what is being received from the user";
-        memcpy(input_stream, temp, strlen(temp));
+	_Nt_array_ptr<char> temp : byte_count(44) = "This is what is being received from the user";
+        memcpy<char>(_Assume_bounds_cast<_Array_ptr<char>>(input_stream, byte_count(100)), _Assume_bounds_cast<_Nt_array_ptr<char>>(temp, count(44)), 44);
 	return;
 }
 
-void print_output_stream(char* output_stream, ImageHeader* header)
-{
-	printf("Image pixels: ");
+void print_output_stream(_Array_ptr<char> output_stream, _Ptr<ImageHeader> header)
+_Checked {
+	_Unchecked { printf("Image pixels: "); };
   	for (int i = 0; i < header->height; i++)
         {
             for(int  j = 0; j < header->width;j++)
             {
                 int index = i * header->width + j;
-                printf("%d ", output_stream[index]);
+                _Unchecked { printf("%s ", _Assume_bounds_cast<_Array_ptr<char>>(output_stream[index], count(100))); };
             }
-            printf("\n");
+            _Unchecked { printf("\n"); };
         }
-        printf("\n");
+        _Unchecked { printf("\n"); };
 	return;
 }
 
@@ -33,13 +33,13 @@ int main()
 	//initialize the global sandbox
 	CreateSandbox();
 	//create a buffer for input bytes (this is something you gotta load from the argument)
-        char* input_stream = (char*)calloc(1000,sizeof(char));
-        if(input_stream == NULL){
-                printf("Error: %s \n", PROGRAM_STATUS_MSG[MEMORY_ALLOC_ERR_MSG]);
+        char* input_stream = (char*)calloc<char>(1000,sizeof(char));
+        if(input_stream == NULL)_Checked {
+                _Unchecked { printf("Error: %s \n", PROGRAM_STATUS_MSG[MEMORY_ALLOC_ERR_MSG]); };
                 return -1;
         }
 	
-	get_image_bytes(input_stream);
+	get_image_bytes(_Assume_bounds_cast<_Array_ptr<char>>(input_stream, bounds(unknown)));
 	invoke_unchecked_image_load(input_stream);	
 	return 0;
 }
